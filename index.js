df84ac6ad9661c4e65965e7f0f4bd2d59fc77d4d
myruldeen <funstudy10@gmail.com>
2020-04-02 05:18:34 +0800

    update dependencies

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-'use-strict';
+'use strict';
+
 const fs = require('fs');
-const aedes = require('aedes')();
-const port = 8883;
+const instance = require('aedes')();
+const logging = require('aedes-logging');
 
 const options = {
     key: fs.readFileSync('certs/key.pem'),
     cert: fs.readFileSync('certs/certificate.pem')
 };
+const servers = [startTLS()];
 
-const server = require('tls').createServer(options, aedes.handle);
-
-server.listen(port, function() {
-    console.log('server started and listening on port ', port);
+logging({
+    instance: instance,
+    servers: servers
 });
+
+function startTLS() {
+    return require('tls')
+        .createServer(options, instance.handle)
+        .listen(8883);
+}
